6a5fe3568b9479c86f0d761a9f27ef6392622315
Eduardo Ellery <eduellery@gmail.com>
2019-02-02 00:18:45 +0100

    Day 1 - Part 2

--- 2015/python/01/day01.py
+++ 2015/python/01/day01.py
@@ -1,11 +1,17 @@
 chars = list(open('day01.in').read())
 
 count = 0
+index = 0
+basementIndex = 0
 
 for c in chars:
+    index +=1
     if c is '(':
         count += 1
     elif c is ')':
         count -= 1
+    if basementIndex is 0 and count is -1:
+        basementIndex = index
 
 print('P1:', count)
+print('P2:', basementIndex)
