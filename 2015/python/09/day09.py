557db574a985613a89f3a6f169c1b7f667aefdbc
Eduardo Ellery <eduellery@gmail.com>
2019-02-09 14:52:51 +0100

    Day 09

---
+++ 2015/python/09/day09.py
@@ -0,0 +1,30 @@
+import re
+import itertools
+
+lines = filter(None, open('day09.in').read().split('\n'))
+
+distances = {}
+cities = set()
+
+for line in lines:
+    travel = re.findall(r'(.*)\sto\s(.*)\s=\s(.*)', line)
+    for orig, dest, dist in travel:
+        distances[orig, dest] = dist
+        distances[dest, orig] = dist
+        cities.add(orig)
+        cities.add(dest)
+
+minimum = 1000000000
+maximum = 0
+
+for path in itertools.permutations(cities):
+    current = 0
+    for i in range(1, len(path)):
+        current += int(distances[path[i-1], path[i]])
+    if current < minimum:
+        minimum = current
+    if current > maximum:
+        maximum = current
+
+print('P1', minimum)
+print('P2', maximum)
