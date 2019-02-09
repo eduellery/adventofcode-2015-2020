46876569ec45082def5b9bac6d40fcc9b73d875b
Eduardo Ellery <eduellery@gmail.com>
2019-02-09 10:11:10 +0100

    Day 08

---
+++ 2015/python/08/day08.py
@@ -0,0 +1,17 @@
+import re
+
+lines = open('day08.in').read().split()
+
+count = 0
+decod = 0
+encod = 0
+
+for line in lines:
+    count += len(line)
+    decoded = bytes(line, "utf-8").decode("unicode_escape")
+    encoded = re.escape(line)
+    decod += len(decoded) - 2 # Subtract ""
+    encod += len(encoded) + 2 # Add ""
+
+print('P1:', count - decod)
+print('P2:', encod - count)
