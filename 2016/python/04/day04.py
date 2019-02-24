416e9de5eba43aa3765cf09c28f2902f5e44b362
Eduardo Ellery <eduellery@gmail.com>
2019-02-24 23:03:24 +0100

    Day 04

---
+++ 2016/python/04/day04.py
@@ -0,0 +1,25 @@
+import re, collections, string
+
+values = open('day04.in').read()
+
+def caesar_cipher(shift):
+    az = string.ascii_lowercase
+    nshift = shift % len(az)
+    return str.maketrans(az, az[nshift:] + az[:nshift])
+
+p1 = 0
+p2 = None
+
+for encoded, sid, checksum in re.findall(r'([a-z-]+)(\d+)\[(\w+)\]', values):
+    sid = int(sid)
+    letters = ''.join(c for c in encoded if c in string.ascii_lowercase)
+    most = [(-n,c) for c,n in collections.Counter(letters).most_common()]
+    result = ''.join(c for n,c in sorted(most))
+    if result.startswith(checksum):
+        p1 += sid
+        decoded = encoded.translate(caesar_cipher(sid))
+        if 'north' in decoded:
+            p2 = sid
+
+print('P1:', p1)
+print('P2:', p2)
