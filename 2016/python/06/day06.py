0cc27745145b6699e8a309badf27c5abb567ffed
Eduardo Ellery <eduellery@gmail.com>
2019-02-27 23:12:05 +0100

    Day 06

---
+++ 2016/python/06/day06.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+values = open('day06.in').read().splitlines()
+
+count = [Counter(x).most_common() for x in zip(*values)]
+
+print('P1:',''.join(x[0][0] for x in count))
+print('P2:',''.join(x[-1][0] for x in count))
