53136f67c35316312b4db503920280c29542f3c8
Eduardo Ellery <eduellery@gmail.com>
2019-04-07 17:21:20 +0200

    Day 04 - part 2

--- 2017/java/d04/Day04.java
+++ 2017/java/d04/Day04.java
@@ -11,12 +11,15 @@ public class Day04 {
 		try {
 			Scanner in = new Scanner(new File("d04/day04.in"));
 			int p1 = 0;
+			int p2 = 0;
 			while(in.hasNext()) {
 				String line = in.nextLine();
 				Supplier<Stream<String>> supplier = () -> Arrays.stream(line.split("\\s"));
 				p1 += supplier.get().count() == supplier.get().distinct().count() ? 1 : 0;
+				p2 += supplier.get().count() == supplier.get().map(str -> str.chars().sorted().collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString()).distinct().count() ? 1 : 0;
 			}
 			System.out.println("P1: " + p1);
+			System.out.println("P2: " + p2);
 		} catch(Exception e) {
 			System.err.println(e.getMessage());
 		}
