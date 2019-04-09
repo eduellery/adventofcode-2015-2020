400bd39dca7b00c941345393aa7c2831b6aeefd2
Eduardo Ellery <eduellery@gmail.com>
2019-04-09 18:41:29 +0200

    Day 05 - part 2

--- 2017/java/d05/Day05.java
+++ 2017/java/d05/Day05.java
@@ -20,7 +20,16 @@ public class Day05 {
 				idx += jump;
 				p1++;
 			}
+			int p2 = 0; idx = 0;
+			int[] p2Values = values.clone();
+			while (idx < p2Values.length && idx >= 0) {
+				int jump = p2Values[idx];
+				p2Values[idx] += jump >= 3 ? -1 : 1;
+				idx += jump;
+				p2++;
+			}
 			System.out.println("P1: " + p1);
+			System.out.println("P2: " + p2);
 		} catch(Exception e) {
 			System.err.println(e.getMessage());
 		}
