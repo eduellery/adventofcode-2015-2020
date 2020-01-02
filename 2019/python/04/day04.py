cbb6e266740d86e6639c28c1ac4dfbec5dcd6d3a
Eduardo Ellery <eduellery@gmail.com>
2020-01-02 07:05:21 +0100

    2019 - Day 04 - part 2

--- 2019/python/04/day04.py
+++ 2019/python/04/day04.py
@@ -1,11 +1,16 @@
-P1 = []
+P1 = set()
+P2 = set()
 for number in range(138241,674035):
     string = str(number)
     if (list(string) == sorted(string)):
         for digit in string:
             count = string.count(digit)
-            if count >= 2:
-                P1.append(string)
+            if count == 2:
+                P2.add(string)
+                P1.add(string)
                 break
+            elif count > 2:
+                P1.add(string)
 
 print('P1:', len(P1))
+print('P2:', len(P2))
