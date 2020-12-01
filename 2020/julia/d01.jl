d8db4fc158b31570bf70e1c7c97afe67f20f38b5
eduellery <eduellery@gmail.com>
2020-12-01 09:07:05 +0100

    2020 - Day 01 - Part 2

--- 2020/julia/d01.jl
+++ 2020/julia/d01.jl
@@ -4,7 +4,7 @@ using Base.Iterators
 values = readdlm("d01.in", '\t', Int, '\n')
 sum = 2020
 
-function matching_sum(numbers, sum)
+function matching_2sum(numbers, sum)
     for (a, b) in product(numbers, numbers)
         if a + b == sum
             return a * b
@@ -12,4 +12,13 @@ function matching_sum(numbers, sum)
     end
 end
 
-println("P1: ", matching_sum(values, sum))
+function matching_3sum(numbers, sum)
+    for (a, b, c) in product(numbers, numbers, numbers)
+        if a + b +c == sum
+            return a * b * c
+        end
+    end
+end
+
+println("P1: ", matching_2sum(values, sum))
+println("P2: ", matching_3sum(values, sum))
