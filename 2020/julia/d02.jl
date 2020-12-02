557616769b27b06023cd78857e5c07ab9651783d
eduellery <eduellery@gmail.com>
2020-12-02 08:42:47 +0100

    2020 - Day 02 - Part 2

--- 2020/julia/d02.jl
+++ 2020/julia/d02.jl
@@ -3,6 +3,7 @@ using DelimitedFiles
 lines = readdlm("d02.in", ' ', String)
 length = size(lines, 1)
 P1 = 0
+P2 = 0
 
 for i in 1:length
     nums = parse.(Int, split(lines[i,1], '-'))
@@ -12,6 +13,10 @@ for i in 1:length
     if nums[1] <= x <= nums[2]
         global P1 += 1
     end
+    if xor(pass[nums[1]] == char, pass[nums[2]] == char)
+        global P2 += 1
+    end
 end
 
 println("P1: ", P1)
+println("P2: ", P2)
