f5d42b911761d92c540af1bc668cc6493e323645
eduellery <eduellery@gmail.com>
2020-12-06 10:19:21 +0100

    2020 - Day 06 - Part 2

--- 2020/julia/d06.jl
+++ 2020/julia/d06.jl
@@ -1,18 +1,28 @@
 lines = readlines("d06.in")
 append!(lines, [""])
 
-P1 = 0
+P1, P2 = 0, 0
+groupanswers = Set()
+individualanswers = []
 
-answers = Set()
 for line in lines
     if line == ""
-        global P1 += length(answers)
-        global answers = Set()
-    else
-        for char in line
-            push!(answers, char)
+        global P1 += length(groupanswers)
+        for answer in individualanswers
+            intersect!(groupanswers, answer)
         end
+        global P2 += length(groupanswers)
+        global groupanswers = Set()
+        global individualanswers = []
+    else
+        answer = Set()
+        for char in line
+            push!(groupanswers, char)
+            push!(answer, char)
+        end
+        push!(individualanswers, answer)
     end
 end
 
 println("P1: ", P1)
+println("P2: ", P2)
