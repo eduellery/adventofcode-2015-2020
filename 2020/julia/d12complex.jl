0696f3935c95b5efa106cb9f39baceac24e151f3
eduellery <eduellery@gmail.com>
2020-12-12 22:21:06 +0100

    2020 - Day 12 - Refactor, gorgeous

--- 2020/julia/d12complex.jl
+++ 2020/julia/d12complex.jl
@@ -1,25 +1,20 @@
 function move(instructions, dir = Complex(1, 0), way = Complex(10, 1), pos = Complex(0, 0))
-    pos1 = 0
-    pos2 = pos
+    pos1 = pos2 = pos
     directions = Dict([("N", Complex(0, 1)), ("S", Complex(0, -1)), ("E", Complex(1, 0)), ("W", Complex(-1, 0))])
     for instruction in instructions
-        (cmd, stringval) = match(r"([NSEWLRF])([0-9]+)", instruction).captures
-        val = parse(Int, stringval)
+        (cmd, num) = match(r"([NSEWLRF])([0-9]+)", instruction).captures
+        val = parse(Int, num)
         if cmd == "F"
-            pos1 += dir * val
-            pos2 += way * val
+            pos1, pos2 = map(x -> x[1] += x[2] * val, [[pos1, dir], [pos2, way]])
         elseif cmd == "L"
-            dir *= Complex(0, 1)^div(val, 90)
-            way *= Complex(0, 1)^div(val, 90)
+            dir, way = map(x -> x *= Complex(0, 1)^div(val, 90), [dir, way])
         elseif cmd == "R"
-            dir *= Complex(0, -1)^div(val, 90)
-            way *= Complex(0, -1)^div(val, 90)
+            dir, way = map(x -> x *= Complex(0, -1)^div(val, 90), [dir, way])
         else
-            pos1 += val * get(directions, cmd, nothing)
-            way += val * get(directions, cmd, nothing)
+            pos1, way = map(x -> x += val * get(directions, cmd, nothing), [pos1, way])
         end
     end
-    return (abs(real(pos1)) + abs(imag(pos1)), abs(real(pos2)) + abs(imag(pos2)))
+    return map(x -> abs(real(x)) + abs(imag(x)), [pos1, pos2])
 end
 
 instructions = readlines("d12.in")
