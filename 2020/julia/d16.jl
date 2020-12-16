6652e25c0209bd5a59588ef32c0ac744ff735ac5
eduellery <eduellery@gmail.com>
2020-12-16 20:57:12 +0100

    2020 - Day 16 - Part 2

--- 2020/julia/d16.jl
+++ 2020/julia/d16.jl
@@ -11,8 +11,12 @@ function tointarray(value)
     map(x -> parse(Int,x), split(value, ","))
 end
 
+function inrange(rule, field)
+    return field in rule[1] || field in rule[2]
+end
+
 function rulesapplies(rules, field)
-    return any(r -> (field in r[1] || field in r[2]), values(rules))
+    return any(rule -> inrange(rule, field), values(rules))
 end
 
 function findinvalid(rules, nearbytickets)
@@ -29,6 +33,40 @@ function findinvalid(rules, nearbytickets)
     return invalidtickets, invalidfields
 end
 
+function rulesbyposition(rules, myticket, validtickets)
+    possibilities = Dict()
+    for i in 1:length(myticket)
+        for (label, rule) in rules
+            if all(field -> inrange(rule, field), ticket[i] for ticket in validtickets)
+                possibilities[i] = push!(get(possibilities, i, String[]), label)
+            end
+        end
+    end
+    return possibilities
+end
+
+function rulesorder(possiblerules)
+    rulespositions = Dict()
+    while !isempty(possiblerules)
+        for (i, labels) in possiblerules
+            if length(labels) == 1
+                label = labels[1]
+                rulespositions[label] = i
+                pop!(possiblerules, i)
+                for (i, labels) in possiblerules
+                    setdiff!(labels, [label])
+                end
+                break
+            end
+        end
+    end
+    return rulespositions
+end
+
+function findfields(myticket, rulespositions, searchstring)
+    return collect(myticket[i] for (label, i) in rulespositions if contains(label, searchstring))
+end
+
 input = readinput("d16.in")
 
 rules = parserules(input[1])
@@ -36,5 +74,10 @@ myticket = tointarray(input[2][2])
 nearbytickets = Dict((index, val) for (index, val) in enumerate(map(x -> tointarray(x), input[3][2:end])))
 
 invalidtickets, invalidfields = findinvalid(rules, nearbytickets)
+validtickets = setdiff(values(nearbytickets), invalidtickets)
+possiblerules = rulesbyposition(rules, myticket, validtickets)
+rulespositions = rulesorder(possiblerules)
+departurefields = findfields(myticket, rulespositions, "departure")
 
 println("P1: ", sum(invalidfields))
+println("P2: ", prod(departurefields))
