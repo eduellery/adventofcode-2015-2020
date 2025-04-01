40fb1342b9895e50ea6850dff9324d2d705f6ceb
Eduardo Ellery <eduellery@gmail.com>
2025-04-01 18:34:29 +0200

    Removing files from 2019

--- 2019/python/d05.py
+++
@@ -1,9 +0,0 @@
-import intcode
-
-values = list(map(int,(open('d05.in').read().split(','))))
-
-_, P1 = intcode.IntCode().compute(values.copy())
-_, P2 = intcode.IntCode().compute(values.copy(), system_id = 5)
-
-print('P1:', P1)
-print('P2:', P2)
