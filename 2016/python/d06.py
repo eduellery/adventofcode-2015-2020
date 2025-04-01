db7edeee6b27df54949376aa6b513ede904eebb2
Eduardo Ellery <eduellery@gmail.com>
2025-04-01 18:29:56 +0200

    Removing files from 2016

--- 2016/python/d06.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-
-values = open('d06.in').read().splitlines()
-
-count = [Counter(x).most_common() for x in zip(*values)]
-
-print('P1:',''.join(x[0][0] for x in count))
-print('P2:',''.join(x[-1][0] for x in count))
