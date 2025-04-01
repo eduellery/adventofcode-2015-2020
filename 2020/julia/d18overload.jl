903335d39d18b178c913eddde77c245e58cc3902
Eduardo Ellery <eduellery@gmail.com>
2025-04-01 18:45:43 +0200

    Removing files from 2020

--- 2020/julia/d18overload.jl
+++
@@ -1,9 +0,0 @@
-⊗(x,y)=x+y
-⊕(x,y)=x*y
-lines = readlines("d18.in")
-linesp1 = replace.(lines, "*"=>"⊕")
-linesp2 = replace.(linesp1, "+"=>"⊗")
-results1 = @. eval(Meta.parse(linesp1))
-results2 = @. eval(Meta.parse(linesp2))
-println("P1: ", sum(results1))
-println("P2: ", sum(results2))
